64985fd7a87892c74340d596fae224f1daeccaad
DanielRatti <danielpr55rr@gmai.com>
2020-12-01 15:34:08 -0300

    resolução exercicios e desafios

---
+++ semana3/operadores/js-vanilla-template/src/index.js
@@ -0,0 +1,124 @@
+// INTERPRETAÇÃO
+
+// EXERC 1
+
+//a- false
+//b- false
+//c- true
+//d- Boolean
+
+// EXERC 2
+
+// a- undefined
+// b- null 
+// c- 11
+// d- 3
+// e- [3, 19, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+// f- 9
+
+// ESCRITA 
+
+// EXERC 1
+
+// const idade = prompt("Qual a sua idade?")
+// let idadeAmigo = prompt("Qual a idade do seu melhor amigo/amiga?")
+
+// const idadeMaior = idade > idadeAmigo
+// console.log(`sua idade é maior que a do seu melhor amigo/amiga?`, idadeMaior)
+
+// const resultado = idade - idadeAmigo
+// console.log(resultado)
+
+// EXERC 2
+
+// const par = Number(prompt("Insira um número par"))
+// let resultado = par%2
+// console.log(resultado)
+
+// ao ensirir o número par ele da como resto zero e se o número for impra 1
+
+// EXERC 3
+
+// let listaDeTafefas = []
+// const pergunta1 = prompt("1 tarefa que você precisa realizar no dia")
+// const pergunta2 = prompt("2 tarefa que você precisa realizar no dia")
+// const pergunta3 = prompt("3 tarefa que você precisa realizar no dia")
+
+// listaDeTafefas.push(pergunta1, pergunta2, pergunta3)
+// console.log(listaDeTafefas)
+
+// const indice = prompt("Digite a tarefa que ja realizou entre 0, 1 e 2")
+// const indiceNumero = Number(indice)
+// listaDeTafefas.splice(indiceNumero, 1)
+// console.log(listaDeTafefas)
+
+// EXERC 4
+
+// let nome = prompt("Qual o seu nome?")
+// let email = prompt("Qual o seu e-mail?")
+// console.log(`o e-mail ${email} foi cadastrado com sucesso. Seja bem vinda(o), ${nome} !`)
+
+// DESAFIOS
+
+// EXERC 1
+
+// let fahretigh = (77 - 32) * 5/9 + 273.15
+// console.log("77°F EM K°:", fahretigh)
+
+// let celsus = (80) * 9 / 5 + 32
+// console.log("80°C em F°:" ,celsus)
+
+// let f1 = (30) * 9/5 + 32
+// let k1 = 30 + 273.15
+// console.log("30°C EM F° e K°:", f1, k1)
+
+// let valorCelsius = Number (prompt("Insira a temperatura em Celsius que quer converter:"))
+// let valorFah = (valorCelsius) * 9/5 +32
+// let valorKel = valorCelsius + 273.15
+// console.log(` O valor ${valorCelsius}°C convertido para F° é ${valorFah} e para Kelvin é ${valorKel}`)
+
+//EXERC 2
+
+// let custo = 0.05
+// let gasto =280
+// let mult = gasto * custo
+
+// console.log("Valor a ser pago por hora:", mult)
+
+// let desconto = mult*0.15
+// let total = mult - desconto
+
+// console.log("Valor a ser pago com desconto de 15%:", total)
+
+// EXERC 4
+
+// let lbPeso = 20 / 2.205
+// console.log(`20lb equivale a ${lbPeso} KG`)
+
+// let ozKg = 10.5 / 35.274
+// console.log(`10.5oz equivale a ${ozKg} KG`)
+
+// let milhaMetro = 100 * 1609
+// console.log(`100mi equivale a ${milhaMetro} M`)
+
+// let ftMetro = 50 / 3.281
+// console.log(`50ft equivale a ${ftMetro} M`)
+
+// let gaLitro = 103.56 / 3.785
+// console.log(`20lb equivale a ${lbPeso} L`)
+
+// let xicaLitro = 450 / 2.205
+// console.log(`20lb equivale a ${xicaLitro} L`)
+
+// let pedir = Number (prompt("Insira o valor em Libras para ser convertido em kilos:"))
+// let lb = pedir / 2.205
+// console.log(`O valor de ${pedir}Lb para KG é: ${lb} `)
+
+
+
+
+
+
+
+
+
