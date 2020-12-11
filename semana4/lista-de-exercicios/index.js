25ddf355098ee55aaf23342e1725c87d65d55237
DanielRatti <danielpr55rr@gmai.com>
2020-12-11 11:12:25 -0300

    Exercicio 3 interpretação

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -7,3 +7,6 @@
 // o primero logo esta fazendo a multiplicaçao dos valores e da o resultado,
 // ja o segundo log da "tipo de ivestimento incorreto" pois tesouro direto não,
 // é um tipo de investimento.
+
+//3. o array1 esta guardando somente os numeros pares e o array2 os numeros impares,
+// os 3 consoles.log estao mostrando a quantidade total de numeros e o tamanho do array1 e 2.
