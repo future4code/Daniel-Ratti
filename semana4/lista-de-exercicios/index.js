c826a31fb778985a8475b8939b217819d181c922
DanielRatti <danielpr55rr@gmai.com>
2020-12-11 11:04:38 -0300

    Exercicio 2 interpretação

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -1,3 +1,9 @@
 //INTERPRETAÇÃO DE CÓDIGO
 
 //1. Esta fazendo a conversão de real para dolar.
+
+//2. É uma função para escolher um tipo de carteira de investimento,
+// ele recebe um tipo e um valor e multiplica por um valor ja definido.
+// o primero logo esta fazendo a multiplicaçao dos valores e da o resultado,
+// ja o segundo log da "tipo de ivestimento incorreto" pois tesouro direto não,
+// é um tipo de investimento.
