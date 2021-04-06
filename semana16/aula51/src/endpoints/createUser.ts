510adc740210c03afc2530444b15f94fddc3d294
DanielRatti <danielpr55rr@gmai.com>
2021-04-06 17:08:57 -0300

    AULA 51 - Exercicios do 1 ao 6

---
+++ semana16/aula51/src/endpoints/createUser.ts
@@ -0,0 +1,52 @@
+import { Request, Response } from "express";
+import { generateId } from "../services/idGenerator";
+import { generateToken } from "../services/authenticator";
+import { User } from "../types/user";
+import { hash } from "../services/hashManager";
+import { USER_ROLES } from "../types/user";
+import insertUser from "../data/insertUser";
+
+export default async function createUser(
+  req: Request,
+  res: Response
+): Promise<void> {
+  try {
+    const { email, password, role } = req.body;
+
+    if (!email || email.indexOf("@") === -1) {
+      throw new Error("E-mail inválido!");
+    }
+
+    if (!password || password.length < 6) {
+      throw new Error("A senha deve conter mais de seis digitos!");
+    }
+
+    if (role !== USER_ROLES.ADMIN && role !== USER_ROLES.NORMAL) {
+      throw new Error("ROLE só pode ser ADMIN ou NORMAL!");
+    }
+
+    const id: string = generateId();
+
+    const hashPassword: string = await hash(password);
+
+    const newUser: User = {
+      id,
+      email,
+      password: hashPassword,
+      role,
+    };
+
+    await insertUser(newUser);
+
+    const token = generateToken({
+      id,
+      role: req.body.role,
+    });
+
+    res.status(200).send({ message: "Usuário criado!", token });
+  } catch (error) {
+    res.status(400).send({
+      message: error.message || error.sqlMessage,
+    });
+  }
+}
